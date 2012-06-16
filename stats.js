573d7da9bb8f4e0e05ba7a5fd1317cd1430b3b3d
Henri Bouvier <henri@droctopus.(none)>
2012-06-16 11:45:34 -0400

    Revert "Added support to receive multiple stats in the same UDP DGRAM packet separated by a line-feed"
    This reverts commit e6cc6b9f811a71cdc1afce624292bec7f0fe3dcc.

--- stats.js
+++ stats.js
@@ -86,54 +86,50 @@ config.configFile(process.argv[2], function (config, oldConfig) {
 
     server = dgram.createSocket('udp4', function (msg, rinfo) {
       if (config.dumpMessages) { util.log(msg.toString()); }
-      var vectorOfMessage = msgs.toString().split('\n');
-      var vectorSize = vectorOfMessage.length;
-      for (var index = 0 ; index < vectorSize ; ++index) {
-        var bits = msg.toString().split(':');
-        var key = bits.shift()
-                      .replace(/\s+/g, '_')
-                      .replace(/\//g, '-')
-                      .replace(/[^a-zA-Z_\-0-9\.]/g, '');
-  
-        if (keyFlushInterval > 0) {
-          if (! keyCounter[key]) {
-            keyCounter[key] = 0;
-          }
-          keyCounter[key] += 1;
+      var bits = msg.toString().split(':');
+      var key = bits.shift()
+                    .replace(/\s+/g, '_')
+                    .replace(/\//g, '-')
+                    .replace(/[^a-zA-Z_\-0-9\.]/g, '');
+
+      if (keyFlushInterval > 0) {
+        if (! keyCounter[key]) {
+          keyCounter[key] = 0;
         }
-  
-        if (bits.length == 0) {
-          bits.push("1");
-        }
-  
-        for (var i = 0; i < bits.length; i++) {
-          var sampleRate = 1;
-          var fields = bits[i].split("|");
-          if (fields[1] === undefined) {
-              util.log('Bad line: ' + fields);
-              stats['messages']['bad_lines_seen']++;
-              continue;
-          }
-          if (fields[1].trim() == "ms") {
-            if (! timers[key]) {
-              timers[key] = [];
-            }
-            timers[key].push(Number(fields[0] || 0));
-          } else if (fields[1].trim() == "g") {
-            gauges[key] = Number(fields[0] || 0);
-          } else {
-            if (fields[2] && fields[2].match(/^@([\d\.]+)/)) {
-              sampleRate = Number(fields[2].match(/^@([\d\.]+)/)[1]);
-            }
-            if (! counters[key]) {
-              counters[key] = 0;
-            }
-            counters[key] += Number(fields[0] || 1) * (1 / sampleRate);
-          }
-        }
-  
-        stats['messages']['last_msg_seen'] = Math.round(new Date().getTime() / 1000);
+        keyCounter[key] += 1;
       }
+
+      if (bits.length == 0) {
+        bits.push("1");
+      }
+
+      for (var i = 0; i < bits.length; i++) {
+        var sampleRate = 1;
+        var fields = bits[i].split("|");
+        if (fields[1] === undefined) {
+            util.log('Bad line: ' + fields);
+            stats['messages']['bad_lines_seen']++;
+            continue;
+        }
+        if (fields[1].trim() == "ms") {
+          if (! timers[key]) {
+            timers[key] = [];
+          }
+          timers[key].push(Number(fields[0] || 0));
+        } else if (fields[1].trim() == "g") {
+          gauges[key] = Number(fields[0] || 0);
+        } else {
+          if (fields[2] && fields[2].match(/^@([\d\.]+)/)) {
+            sampleRate = Number(fields[2].match(/^@([\d\.]+)/)[1]);
+          }
+          if (! counters[key]) {
+            counters[key] = 0;
+          }
+          counters[key] += Number(fields[0] || 1) * (1 / sampleRate);
+        }
+      }
+
+      stats['messages']['last_msg_seen'] = Math.round(new Date().getTime() / 1000);
     });
 
     mgmtServer = net.createServer(function(stream) {
